162d43c9c3004f34baecf0eb0c2a51678260e1c7
Nguyen Xuan Mi <mi.nguyenk22cse@hcmut.edu.vn>
2025-12-01 13:21:42 +0700

    feat (scripts): setup Shop and ShopItem scripts (not finished)

---
+++ Assets/Scripts/Main/Shop/Shop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Main.Shop
+{
+    public class Shop : MonoBehaviour
+    {
+        public static Shop Instance;
+        
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        private void UpdateData()
+        {
+            
+        }
+
+        public void Purchase(int itemId)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Main/Shop/ShopItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Main.Shop
+{
+    public class ShopItem : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup lockCanvasGroup;
+        [SerializeField] private Button shopItemButton;
+        [SerializeField] private TMP_Text priceText;
+        [SerializeField] private Image itemImage;
+        
+        private bool _isPurchased;
+        private int _itemId;
+
+        public void SetData()
+        {
+            
+        }
+
+        private void SetItemUnpurchasable()
+        {
+            lockCanvasGroup.alpha = 0.5f;
+            lockCanvasGroup.interactable = true;
+            lockCanvasGroup.blocksRaycasts = true;
+        }
+
+        private void SetItemPurchasable()
+        {
+            lockCanvasGroup.alpha = 0f;
+            lockCanvasGroup.interactable = false;
+            lockCanvasGroup.blocksRaycasts = false;
+        }
+
+        private void SetItemPurchased()
+        {
+            lockCanvasGroup.alpha = 1f;
+            lockCanvasGroup.interactable = true;
+            lockCanvasGroup.blocksRaycasts = true;
+        }
+
+        private void Awake()
+        {
+            shopItemButton.onClick.AddListener(OnClickShopItem);
+        }
+
+        private void OnDestroy()
+        {
+            shopItemButton.onClick.RemoveListener(OnClickShopItem);
+        }
+
+        private void OnClickShopItem()
+        {
+            
+        }
+    }
+}
